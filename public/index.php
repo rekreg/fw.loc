3814f103e24c79b2ca79044afa1c1bb824b2cf46
Andrew Nikolaev <andrew@iMac-Andrew.local>
2020-08-25 16:43:18 +0300

    Сохраняем 25.08.2020 16:43

--- public/index.php
+++ public/index.php
@@ -1,4 +1,23 @@
 <?php
 
 
-echo __FILE__;
+$query = rtrim($_SERVER['QUERY_STRING'], '/');
+
+require "../vendor/libs/functions.php";
+require "../vendor/core/Router.php";
+
+
+Router::add('posts/add', ['controller' => 'Posts', 'action' => 'add']);
+
+Router::add('posts', ['controller' => 'Posts', 'action' => 'index']);
+
+Router::add('', ['controller' => 'Main', 'action' => 'index']);
+
+
+print_arr(Router::getRoutes());
+
+if (Router::matchRoute($query)) {
+  print_arr(Router::getRoute());
+} else {
+  echo "404";
+}
