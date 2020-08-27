7884eb868936636cc74e47211e967a7ea3cb2456
Andrew Nikolaev <andrew@iMac-Andrew.local>
2020-08-27 20:17:49 +0300

    2-й Урок: 21:41

--- public/index.php
+++ public/index.php
@@ -3,21 +3,20 @@
 
 $query = rtrim($_SERVER['QUERY_STRING'], '/');
 
+//echo $query;
+
 require "../vendor/libs/functions.php";
 require "../vendor/core/Router.php";
 
 
-Router::add('posts/add', ['controller' => 'Posts', 'action' => 'add']);
 
-Router::add('posts', ['controller' => 'Posts', 'action' => 'index']);
+Router::add('^$', ['controller' => 'Main', 'action' => 'index']);
 
-Router::add('', ['controller' => 'Main', 'action' => 'index']);
+Router::add('^(?P<controller>[a-z-]+)/?(?P<action>[a-z-]+)?$');
 
 
 print_arr(Router::getRoutes());
 
-if (Router::matchRoute($query)) {
-  print_arr(Router::getRoute());
-} else {
-  echo "404";
-}
+Router::dispatch($query);
+
+
--- vendor/core/Router.php
+++ vendor/core/Router.php
@@ -27,12 +27,29 @@ class Router {
   
   public static function matchRoute($url) {
     foreach(self::$routes as $pattern => $route) {
-      if($url == $pattern) {
+      if(preg_match("#$pattern#i", $url, $matches)) {
+        
+        foreach($matches as $key => $v) {
+          if(is_string($key)) {
+            $route[$key] = $v;
+          }
+        }
+        
         self::$route = $route;
+        print_arr(self::$route);
         return true;
       }
     }
-    return false;
+    return false; 
+  }
+  
+  public static function dispatch($url) {
+    if(self::matchRoute($url)) {
+        echo "OK";
+    } else {
+        http_response_code(404);
+        include "404.html";  
+    }
   }
   
   
